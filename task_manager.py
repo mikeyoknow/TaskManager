21c1dc03209f0c09c9753e289eaa275e12f25950
mikeyoknow <114947956+mikeyoknow@users.noreply.github.com>
2023-07-12 13:43:25 -0400

    created initial files and developed the basic user authintication

---
+++ task_manager.py
@@ -0,0 +1,51 @@
+def user_information(ussnm, pssd) :
+    name = input("Enter you name please: ")
+    address = input("Your address: ")
+    age = input("Your age please: ")
+    ussnm_ = ussnm+" task.txt"
+
+    f = open(ussnm_, 'a')
+    f.write(pssd)
+    f.write("\nName: ")
+    f.write(name)
+    f.write('\n')
+    f.write("Address :")
+    f.write(address)
+    f.write('\n')
+    f.write("Age: ")
+    f.write(age)
+    f.write('\n')
+    f.close()
+
+def login() :
+    print("Please enter your username: ")
+    user_nm = input("Enter here: ")
+
+    pssd_wr = (input("enter the password")) + '\n'
+    try:
+        usernm = user_nm+" task.txt"
+        f_ = open(usernm, 'r')
+        k = f_.readline(0)[0]
+        f_.close()
+
+        if pssd_wr == k:
+            print(
+                "1--To view your data \n2--To add task \n3--Update\
+                    task status \n4--View task status"
+            )
+            a = input()
+        else:
+            print("YOUR PASSSWORD OR USERNAME IS WRONG, TRY AGAIN")
+            login()
+    except Exception as e:
+        print(e)
+        login()
+
+def signup() :
+    print("Please enter the username by which you \
+            wanna access your account")
+    username = input("please enter here: ")
+    password = input("Enter a password: ")
+    user_information(username,password)
+    print("Proceed to login... ")
+    login()
